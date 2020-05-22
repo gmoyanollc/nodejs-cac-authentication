50030b487ff06317faa0b7153f2ae14afee9af99
gmoyanollc <g@gmoyano.com>
2020-05-22 14:12:20 -0700

    cli refactor

--- cac-auth-cli.js
+++ cac-auth-cli.js
@@ -1,70 +1,111 @@
+const tls = require('tls');
 const inquirer = require("inquirer");
 var graphene = require("graphene-pk11");
 var Module = graphene.Module;
 
 var mod = Module.load("/lib64/pkcs11/opensc-pkcs11.so", "opensc");
 
-mod.initialize();
-
-var slots = mod.getSlots(true);
-if (slots.length > 0) {
-  for (var i = 0; i < slots.length; i++) {
-    var slot = slots.items(i);
-    console.log("Slot #" + slot.handle);
-    console.log("\tDescription:", slot.slotDescription);
-    console.log("\tSerial:", slot.getToken().serialNumber);
-    console.log(
-      "\tPassword(min/max): %d/%d",
-      slot.getToken().minPinLen,
-      slot.getToken().maxPinLen
-    );
-    console.log("\tIs hardware:", !!(slot.flags & graphene.SlotFlag.HW_SLOT));
-    console.log(
-      "\tIs removable:",
-      !!(slot.flags & graphene.SlotFlag.REMOVABLE_DEVICE)
-    );
-    console.log(
-      "\tIs initialized:",
-      !!(slot.flags & graphene.SlotFlag.TOKEN_PRESENT)
-    );
-    console.log("\n\nMechanisms:");
-    console.log("Name                       h/s/v/e/d/w/u");
-    console.log("========================================");
-    function b(v) {
-      return v ? "+" : "-";
-    }
-
-    function s(v) {
-      v = v.toString();
-      for (var i_1 = v.length; i_1 < 27; i_1++) {
-        v += " ";
-      }
-      return v;
-    }
-
-    var mechs = slot.getMechanisms();
-    for (var j = 0; j < mechs.length; j++) {
-      var mech = mechs.items(j);
-      console.log(
-        s(mech.name) +
-          b(mech.flags & graphene.MechanismFlag.DIGEST) +
-          "/" +
-          b(mech.flags & graphene.MechanismFlag.SIGN) +
-          "/" +
-          b(mech.flags & graphene.MechanismFlag.VERIFY) +
-          "/" +
-          b(mech.flags & graphene.MechanismFlag.ENCRYPT) +
-          "/" +
-          b(mech.flags & graphene.MechanismFlag.DECRYPT) +
-          "/" +
-          b(mech.flags & graphene.MechanismFlag.WRAP) +
-          "/" +
-          b(mech.flags & graphene.MechanismFlag.UNWRAP)
-      );
-    }
+const TlsOptions = () => {
+  let options = {
+    ca:   "",
+    cert: "",
+    key:  "",
+    host: "",
+    port: 8000,
+    rejectUnauthorized:true,
+    requestCert:true
+  };
+  return {
+    getTlsOptions: () => options,
+    setCa: (ca) => { options.ca = ca },
+    setCert: (cert) => { options.cert = cert },
+    setKey: (key) => { options.key = key },
+    setHost: (host) => { options.host = host },
   }
-  var slot = mod.getSlots(0);
-  if (slot.flags & graphene.SlotFlag.TOKEN_PRESENT) {
+}
+
+const ScReader = () => {
+  let slots
+
+  function printProperties () {
+    //[Listing capabilities](https://www.npmjs.com/package/graphene-pk11)
+    const slotPrintDelimiter = "////////////////////////////////////////////////////////////"  // +g
+
+    for (var i = 0; i < slots.length; i++) {
+      var slot = slots.items(i);
+      console.log(slotPrintDelimiter)
+      console.log("Slot #", i) // +g
+      console.log("Slot Handle #" + slot.handle);
+      console.log("\tDescription:", slot.slotDescription);
+      console.log("\tSerial:", slot.getToken().serialNumber);
+      console.log(
+        "\tPassword(min/max): %d/%d",
+        slot.getToken().minPinLen,
+        slot.getToken().maxPinLen
+      );
+      console.log("\tIs hardware:", !!(slot.flags & graphene.SlotFlag.HW_SLOT));
+      console.log(
+        "\tIs removable:",
+        !!(slot.flags & graphene.SlotFlag.REMOVABLE_DEVICE)
+      );
+      console.log(
+        "\tIs initialized:",
+        !!(slot.flags & graphene.SlotFlag.TOKEN_PRESENT)
+      );
+      console.log("\n\nMechanisms:");
+      console.log("Name                       h/s/v/e/d/w/u");
+      console.log("========================================");
+      function b(v) {
+        return v ? "+" : "-";
+      }
+  
+      function s(v) {
+        v = v.toString();
+        for (var i_1 = v.length; i_1 < 27; i_1++) {
+          v += " ";
+        }
+        return v;
+      }
+  
+      var mechs = slot.getMechanisms();
+      for (var j = 0; j < mechs.length; j++) {
+        var mech = mechs.items(j);
+        console.log(
+          s(mech.name) +
+            b(mech.flags & graphene.MechanismFlag.DIGEST) +
+            "/" +
+            b(mech.flags & graphene.MechanismFlag.SIGN) +
+            "/" +
+            b(mech.flags & graphene.MechanismFlag.VERIFY) +
+            "/" +
+            b(mech.flags & graphene.MechanismFlag.ENCRYPT) +
+            "/" +
+            b(mech.flags & graphene.MechanismFlag.DECRYPT) +
+            "/" +
+            b(mech.flags & graphene.MechanismFlag.WRAP) +
+            "/" +
+            b(mech.flags & graphene.MechanismFlag.UNWRAP)
+        );
+      }
+      console.log(slotPrintDelimiter)
+    }
+
+  }
+  return {
+    getSlots: () => slots, 
+    printProperties: () => printProperties(),
+    setSlots: () => slots = mod.getSlots(true),
+  }
+}
+
+const Sc = (slotNumber) => {
+  const slotKey = slotNumber || 0
+  const slot = mod.getSlots(slotKey)
+
+  let privateCerts
+  let publicCerts = getPublicCerts()
+
+  function scLogin (result) {
     inquirer
       .prompt([
         {
@@ -73,55 +114,13 @@ if (slots.length > 0) {
           name: "passphrase"
         }
       ])
-      .then(answer => {
-        var session = slot.open();
-        let loginResponse = session.login(answer);
-        if (!loginResponse) {
-          let publicKeys = session.find({class: graphene.ObjectClass.PUBLIC_KEY}).items_
-
-          for (let item = 0; (item < publicKeys.length); item++) {
-            console.log(publicKeys[item])
-          }
-
-          /*// generate AES key
-          var key = session.generateKey(graphene.KeyGenMechanism.AES, {
-            class: graphene.ObjectClass.SECRET_KEY,
-            token: false,
-            valueLen: 256 / 8,
-            keyType: graphene.KeyType.AES,
-            label: "My AES secret key",
-            encrypt: true,
-            decrypt: true
-          });
-          console.log(key);
-          // enc algorithm
-          var alg = {
-            name: "AES_CBC_PAD",
-            params: Buffer.from([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6]) // IV
-          };
-          var MESSAGE = "Encrypted message";
-
-          // encrypting
-          var cipher = session.createCipher(alg, key);
-          var enc = cipher.update(MESSAGE);
-          enc = Buffer.concat([enc, cipher.final()]);
-          console.log("Enc:", enc.toString("hex")); // Enc: eb21e15b896f728a4...
-
-          // decrypting
-          var decipher = session.createDecipher(alg, key);
-          var dec = decipher.update(enc);
-          var msg = Buffer.concat([dec, decipher.final()]).toString();
-          console.log("Message:", msg.toString()); // Message: Encrypted message */
-
-          session.logout();
-          session.close();
-          // Get a number of private key objects on token
-          //console.log(session.find({class: graphene.ObjectClass.PRIVATE_KEY}).length);
-        } else
-          console.log(loginResponse)
-        mod.finalize();
+      .then( answer => {
+        console.log("prompted")
+        result(answer)
       })
       .catch(error => {
+        console.log("[ERROR]")
+        console.log(JSON.stringify(error))
         if (error.isTtyError) {
           // Prompt couldn't be rendered in the current environment
         } else {
@@ -130,7 +129,49 @@ if (slots.length > 0) {
         mod.finalize();
       });
   }
+
+  function getPublicCerts () {
+    if (slot.flags & graphene.SlotFlag.TOKEN_PRESENT) {
+      scLogin( answer => {
+          console.log("got it")
+          var session = slot.open();
+          let loginResponse = session.login(answer);
+          if (!loginResponse) {
+            console.log("valid pass")
+            let publicKeys = session.find({class: graphene.ObjectClass.PUBLIC_KEY}).items_
+  
+            for (let item = 0; (item < publicKeys.length); item++) {
+              console.log(publicKeys[item])
+            }
+            session.logout();
+            session.close();
+          } else
+            console.log(loginResponse)
+          mod.finalize();
+        })
+    }
+  }
+  return {
+    setPrivateCerts: () => {},
+  }
+}
+
+mod.initialize();
+
+let scReader = ScReader()
+scReader.setSlots()
+
+if (scReader.getSlots()) {
+  if (scReader.getSlots().length > 0) {
+    scReader.printProperties()
+    let sc = Sc()
+    //mod.finalize()
+    //console.log("done.")
+  } else {
+    console.log("[WARNING] smart card not found.")
+    mod.finalize()
+  }
 } else {
   mod.finalize();
-  console.log("[WARNING] smart card device or card not found.")
+  console.log("[WARNING] smart card reader module not found.")
 }
